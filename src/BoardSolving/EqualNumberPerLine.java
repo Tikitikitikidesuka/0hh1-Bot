d8939ea3858dde4e8a807c75c6e62b6d097c18e3
SneakyGerbil <deesneakygerbil@gmail.com>
2022-04-09 19:19:51 +0200

    Finished EqualNumberPerLine class

--- src/BoardSolving/EqualNumberPerLine.java
+++ src/BoardSolving/EqualNumberPerLine.java
@@ -1,6 +1,7 @@
 package BoardSolving;
 
 import GameBoard.Board;
+import GameBoard.TileType;
 
 public class EqualNumberPerLine extends SolvingStrategy {
     /**
@@ -11,6 +12,71 @@ public class EqualNumberPerLine extends SolvingStrategy {
      */
     @Override
     public int executeSolveStrategy(Board board) {
-        return 0;
+        int filled = 0;
+
+        filled += equalNumberPerLineHorz(board);
+        filled += equalNumberPerLineVert(board);
+
+        return filled;
+    }
+
+    private int equalNumberPerLineHorz(Board board) {
+        int filled = 0;
+
+        int size = board.getSize();
+        for(int y = 0; y < size; y++) {
+            int colorA = 0, colorB = 0;
+            for(int x = 0; x < size; x++) {
+                switch(board.getTileType(x, y)) {
+                    case COLOR_A -> colorA++;
+                    case COLOR_B -> colorB++;
+                }
+            }
+
+            if(colorA == size/2 || colorB == size/2) {
+                TileType fillType;
+                if(colorA == size/2)
+                    fillType = TileType.COLOR_B;
+                else
+                    fillType = TileType.COLOR_A;
+
+                for(int x = 0; x < size; x++) {
+                    if(board.isTileEmpty(x, y))
+                        board.setTileType(x, y, fillType);
+                }
+            }
+        }
+
+        return filled;
+    }
+
+    private int equalNumberPerLineVert(Board board) {
+        int filled = 0;
+
+        int size = board.getSize();
+        for(int x = 0; x < size; x++) {
+            int colorA = 0, colorB = 0;
+            for(int y = 0; y < size; y++) {
+                switch(board.getTileType(x, y)) {
+                    case COLOR_A -> colorA++;
+                    case COLOR_B -> colorB++;
+                }
+            }
+
+            if(colorA == size/2 || colorB == size/2) {
+                TileType fillType;
+                if(colorA == size/2)
+                    fillType = TileType.COLOR_B;
+                else
+                    fillType = TileType.COLOR_A;
+
+                for(int y = 0; y < size; y++) {
+                    if(board.isTileEmpty(x, y))
+                        board.setTileType(x, y, fillType);
+                }
+            }
+        }
+
+        return filled;
     }
 }
